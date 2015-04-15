cd896fe644960dce450b11b67ab57a6fc223f296
patois <dennis@backtrace.de>
2015-04-15 19:20:56 +0200

    update readme, fix python script

--- tools/client.py
+++ tools/client.py
@@ -7,16 +7,16 @@ if len(sys.argv) < 3:
     print "python client.py <ip> <file>\n"
     sys.exit(0)
 
-s = socket.socket()
-host = sys.argv[1]
 port = 80
+host = sys.argv[1]
 pfile = sys.argv[2]
 
+s = socket.socket()
 s.connect((host, port))
 f = open(pfile, "rb")
 buf = f.read()
 f.close();
-if (f and len):
+if (f and len(buf)):
     sent = s.send(buf)
 print "Sent %d bytes\n" % sent
 s.close

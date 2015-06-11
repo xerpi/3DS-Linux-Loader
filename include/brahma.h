0e26bcd50296793c200c520454f27be31727da76
d0k3 <tore.anon@gmail.com>
2015-06-11 22:31:27 +0200

    Allow load ARM9 payload from memory (rebased)
    Useful for when Brahma is used as library
    
    load_arm9_payload_from_mem() now returns the actual result
    
    Sanity checking for data

--- include/brahma.h
+++ include/brahma.h
@@ -3,6 +3,7 @@
 #include "exploitdata.h"
 
 s32 load_arm9_payload (char *filename);
+s32 load_arm9_payload_from_mem (u8* data, u32 dsize);
 void redirect_codeflow (u32 *dst_addr, u32 *src_addr);
 void do_gshax_copy (void *dst, void *src, u32 len);
 void priv_write_four (u32 address);
